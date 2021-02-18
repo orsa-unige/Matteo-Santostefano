169793f1de392bbcd241cbae83f27a20959c2494
serafino1911 <72070650+serafino1911@users.noreply.github.com>
2021-02-18 09:51:22 +0100

    Add files via upload

--- Sky Simulator/MainSky.py
+++ Sky Simulator/MainSky.py
@@ -1,8 +1,7 @@
 import Query as Q
 import SkyConstructer as SC
 from numpy import random
-import astropy.coordinates as coord
-from astropy import units as u
+
 
 def main():
 
@@ -15,13 +14,9 @@ def main():
 
 def Automatics():
 
-    c = input('Sky Coordinates: ')
-    coordinates = coord.SkyCoord(c, frame='icrs', unit=(u.hourangle, u.deg))
+    coordinates = input('Sky Coordinates: ')
 
-    a = random.rand(5)
-    ph = random.randint(360, size = 3)
-
-    SC.datum(coordinates, a[0], a[1], a[2], a[3], a[4], ph[0], ph[1], ph[2])
+    SC.datum(coordinates, 6)
 
 if __name__ == "__main__":
     main()
--- Sky Simulator/Query.py
+++ Sky Simulator/Query.py
@@ -13,105 +13,61 @@ def Coordinator(c, center):
     w.wcs.ctype = ["RA", "DEC"]
     
     x,y = wcs.utils.skycoord_to_pixel(c, w)
-    x = (-x*4389) + 512 #(deg*arc/sec*deg)*arc/sec*pixel+offset
-    y = (-y*4389) + 516
+    x = (x*4389)/2 + 512 #(deg*arc/sec*deg)*arc/sec*pixel+offset
+    y = (y*4389)/2 + 516
 
     return x, y
 
 
 def Data_structure():
+
     Coord_x = []
     Coord_y = []
-    Flux_U = []
-    Flux_B = []
-    Flux_V = []
-    Flux_R = []
-    Flux_I = []
-    Flux_G = []
-    Flux_J = []
-    Flux_H = []
-    Flux_K = []
-    data = [Coord_x, Coord_y, Flux_U, Flux_B, Flux_V, Flux_R, Flux_I,Flux_G,Flux_J,Flux_H,Flux_K]
+    Flux_tot = []
+    data = [Coord_x, Coord_y, Flux_tot]
+
     return data
     
-def query(coordi = coord.SkyCoord("07 59 5.840", "+15 23 29.24", frame = 'icrs', unit=(u.hourangle, u.deg)),
-          radius = (7/60)*u.deg):
+def query(coordi = "07 59 5.840  +15 23 29.24", photo_filters = ['U', 'B', 'V', 'R', 'I', 'G', 'J', 'H','K'], radius = (15.6/60)*u.deg):
     
-    photo_filters = ['U', 'B', 'V', 'R', 'I','G','J','H','K']
+    coordi = coord.SkyCoord(coordi, frame = 'icrs', unit=(u.hourangle, u.deg))
     f =['flux({0})'.format(x) for x in photo_filters]
     [Simbad.add_votable_fields(g) for g in f]
     result_table = Simbad.query_region(coordi, radius)
+    
     center = [0,0]
     center[0] = coordi.ra.degree
     center[1] = coordi.dec.degree
 
-    data=Data_structure()
+    data = Data_structure()
 
-    Stars_number=len(result_table)
-
+    Stars_number = len(result_table)
+    len_filter_list = len(photo_filters)
+    
     for i in range (Stars_number):
         row = result_table[i]
         
         tot = 0
-        for i in range(11,20):  #sum the fluxies
+        for i in range(11, 11+len_filter_list):  #sum the fluxies
             if row[i] is np.ma.masked:
                 row[i] = 0
             tot += row[i]
+        data[2].append(tot)
 
         if tot != 0: #if total flux is 0 the object is not passed
-            ra = hms(row[1])
-            de = dms(row[2])
-            c = coord.SkyCoord(ra, de, frame = 'icrs')
+            c = coord.SkyCoord(row[1], row[2], frame = 'icrs', unit=(u.hourangle, u.deg))##change here
+
             x, y = Coordinator(c, center)
             data[0].append(x)
             data[1].append(y)
-            #the fluxies of different wavelengh 
-            for f in range (11,20):
-                if row[f] is np.ma.masked:
-                    data[f-9].append(0)
-                else:
-                    data[f-9].append(row[f])
-            #TODO multiply the fluxies for the efficiency spectrum
+
+
     return data
 
-def hms(row):
-    s = list(row)
-    s[2] = 'h'
-    for n,i in enumerate(s):
-        if i == ' ':
-            s[n]='m'
 
-    s.append('s')
-    if s.count('m') == 1:
-        f = ''.join(s)
-    else:
-        s.insert(3,'m')
-        s.insert(3,'0')
-        s.insert(3,'0')
-        f = ''.join(s)
-    return f
-
-def dms(row):
-    s= list(row)
-    if s[2] ==' ':
-        s[2] = 'd'
-    else:
-        s[3] = 'd'
-    for n,i in enumerate(s):
-        if i == ' ':
-            s[n]='m'
-    s.append('s')
-    if s.count('m') == 1:
-        f = ''.join(s)
-    else:
-        s.insert(4,'m')
-        s.insert(4,'0')
-        s.insert(4,'0')
-        f = ''.join(s)
-    return f
 
 def main():
-    data=query()
+    data = query()
     print(data)
 
 if __name__ == '__main__':
