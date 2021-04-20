27a30bed15b8443c7ccc2fbb54eb91be7592132c
serafino1911 <72070650+serafino1911@users.noreply.github.com>
2021-04-20 18:05:39 +0200

    Right values

--- Sky Simulator/From_SECONDAR_to_mm.py
+++ Sky Simulator/From_SECONDAR_to_mm.py
@@ -1,6 +1,6 @@
-starter_point = 1880
+starter_point = 1904
 
 secondar = int(input('Valore del secondar: \n'))
-mill = ((starter_point-secondar)/5) *0.001
+mill = ((starter_point-secondar)*5) *0.001
 
 print(mill)
